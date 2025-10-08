34473c9135afe9d9f27eb201496b6f0ce48d21a5
karimalmk <km.almoukhtar@gmail.com>
2025-10-08 23:54:58 +0300

    fixed venv

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kmalmk/portfolio-optimization/.venv
+set -gx VIRTUAL_ENV /home/kmalmk/portfolio-analytics/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
